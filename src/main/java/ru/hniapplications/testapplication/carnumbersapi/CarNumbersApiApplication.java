d85bfbb03b9ad9909839235a75d9cde8b73eeff1
Hadukin-Nik <68010131+Hadukin-Nik@users.noreply.github.com>
2024-09-23 12:50:25 +0300

    Project init

---
+++ src/main/java/ru/hniapplications/testapplication/carnumbersapi/CarNumbersApiApplication.java
@@ -0,0 +1,13 @@
+package ru.hniapplications.testapplication.carnumbersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarNumbersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarNumbersApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hniapplications/testapplication/carnumbersapi/CarNumbersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hniapplications.testapplication.carnumbersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarNumbersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
