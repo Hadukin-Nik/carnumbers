07f339bfe8e2800bf38b6504868bcdd522731666
Hadukin-Nik <68010131+Hadukin-Nik@users.noreply.github.com>
2024-09-23 22:47:55 +0300

    added a mixed dots adding test

--- src/test/java/ru/hniapplications/testapplication/carnumbersapi/services/CarNumberTreeSetTest.java
+++ src/test/java/ru/hniapplications/testapplication/carnumbersapi/services/CarNumberTreeSetTest.java
@@ -15,7 +15,7 @@ class CarNumberTreeSetTest {
         Assertions.assertNotNull(set);
     }
     @Test
-    public void add_tod_between_L() {
+    public void add_dot_between_L() {
         //Arrange
         CarNumber carNumberL = new CarNumber("СВВ", 300);
         CarNumber carNumberB = new CarNumber("СВВ", 301);
@@ -31,7 +31,7 @@ class CarNumberTreeSetTest {
         Assertions.assertEquals(expectedCarNumber, set.addFirstEntry(carNumberL));
     }
     @Test
-    public void add_tod_between_B() {
+    public void add_dot_between_B() {
         //Arrange
         CarNumber carNumberL = new CarNumber("СВВ", 300);
         CarNumber carNumberB = new CarNumber("СВВ", 301);
@@ -47,7 +47,7 @@ class CarNumberTreeSetTest {
         Assertions.assertEquals(expectedCarNumber, set.addFirstEntry(carNumberB));
     }
     @Test
-    public void add_tod_between_R() {
+    public void add_dot_between_R() {
         //Arrange
         CarNumber carNumberL = new CarNumber("СВВ", 300);
         CarNumber carNumberB = new CarNumber("СВВ", 301);
@@ -64,7 +64,7 @@ class CarNumberTreeSetTest {
     }
 
     @Test
-    public void add_tod_after() {
+    public void add_dot_after() {
         //Arrange
         CarNumber carNumberL = new CarNumber("СВВ", 300);
         CarNumber carNumberB = new CarNumber("СВВ", 301);
@@ -81,7 +81,7 @@ class CarNumberTreeSetTest {
     }
 
     @Test
-    public void add_tod_before() {
+    public void add_dot_before() {
         //Arrange
         CarNumber carNumberL = new CarNumber("СВВ", 300);
         CarNumber carNumberB = new CarNumber("СВВ", 301);
@@ -95,5 +95,31 @@ class CarNumberTreeSetTest {
         set.addCarNumber(carNumberR);
         //Assert
         Assertions.assertEquals(mock, set.addFirstEntry(mock));
+    }
+
+    @Test
+    public void mixed_dot_tests() {
+        //Arrange
+        CarNumber[] carNumbers = new CarNumber[3];
+        carNumbers[0] = new CarNumber("СВВ", 300);
+        carNumbers[1] = new CarNumber("СВВ", 301);
+        carNumbers[2] = new CarNumber("СВВ", 302);
+
+        CarNumber expectedCarNumber = new CarNumber("СВВ", 303);
+        for(int i = 0; i < carNumbers.length; i++) {
+            for(int j = 0; j < carNumbers.length; j++) {
+                for(int k = 0; k < carNumbers.length; k++) {
+                    if(k != j && j != i && k != i) {
+                        //Act
+                        CarNumberTreeSet set = new CarNumberTreeSet();
+                        set.addCarNumber(carNumbers[0]);
+                        set.addCarNumber(carNumbers[1]);
+                        set.addCarNumber(carNumbers[2]);
+                        //Assert
+                        Assertions.assertEquals(expectedCarNumber, set.addFirstEntry(carNumbers[i]));
+                    }
+                }
+            }
+        }
     }
 }
